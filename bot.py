68cd43ea08da16f7772dd7b6cc3d3dd12c77f883
SchneiderNils <95985168+SchneiderNils@users.noreply.github.com>
2022-01-18 18:51:45 +0100

    v1.0.0

---
+++ bot.py
@@ -0,0 +1,15 @@
+#####################################
+#                                   #
+#       Version: v1.0.0             #
+#       Creater: Nils Schneider     #
+#                                   #
+#       Bearbeitet: 18.01.2022      #
+#           ToDo Liste:             #
+#      https://orburl.de/6UHi3H     #
+#                                   #
+#####################################
+
+
+from api import *
+prüfung()
+senden()
