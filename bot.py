c7d33816a629aa84b9709c0771f47a01a7966586
SchneiderNils <95985168+SchneiderNils@users.noreply.github.com>
2022-01-18 19:09:30 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,8 +8,18 @@
 #      https://orburl.de/6UHi3H     #
 #                                   #
 #####################################
-
-
 from api import *
-prüfung()
-senden()
+
+while True:
+    prüfung()
+    senden()
+    print(f"{bcolors.HEADER}Nächste Prüfung in 10 Minuten!{bcolors.ENDC}")
+    time.sleep(300)
+    print(f"{bcolors.HEADER}Nächste Prüfung in 5 Minuten!{bcolors.ENDC}")
+    time.sleep(240)
+    print(f"{bcolors.HEADER}Nächste Prüfung in 1 Minuten!{bcolors.ENDC}")
+    time.sleep(50)
+    print(f"{bcolors.HEADER}Nächste Prüfung in 10 Sekunden!{bcolors.ENDC}")
+    print(bcolors.LINE)
+    time.sleep(10)
+    print("\n\n\n\n\n")
